9342a3c9ac8c6d30b56183016a1ae5a6b5d9b085
Arthur Lanzoni <arthurlanzoni08@gmail.com>
2025-08-08 19:28:30 -0300

    fix: ajustar nome do método getClassificacao (antes getClassificao) na interface Classificavel e classes que a implementam

---
+++ br/com/alura/screenmatch/modelos/Episodio.java
@@ -0,0 +1,58 @@
+package br.com.alura.screenmatch.modelos;
+
+import br.com.alura.screenmatch.calculos.Classificavel;
+
+public class Episodio implements Classificavel {
+
+    private int numero;
+    private String nome;
+    private Serie serie;
+    private int totalVisualizacoes;
+
+    // GETTERS
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Serie getSerie() {
+        return serie;
+    }
+
+    public int gettotalVisualizacoes() {
+        return totalVisualizacoes;
+    }
+
+    // SETTERS
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSerie(Serie serie) {
+        this.serie = serie;
+    }
+
+    public void setTotalVisualizacoes(int totalVisualizacoes) {
+        this.totalVisualizacoes = totalVisualizacoes;
+    }
+
+    @Override
+    public int getClassificao() {
+        if (totalVisualizacoes > 100) {
+            return 4;
+        } else {
+            return 2;
+        }
+
+    }
+
+}
